75d6f1d243b5fcc02a3ba59baf982df203c107e3
Matheus Vinicius <matheus_viniciuss@outlook.com>
2022-12-05 13:07:11 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,10 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["Developer"] = "Matheus Vinicius Silva";
+
+            ViewData["Email"] = "matheus_viniciuss@Outlook.com";
 
             return View();
         }
