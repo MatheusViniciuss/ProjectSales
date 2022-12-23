6151346d9add913c9843fa8de68f9f8fc9c0a2f1
Matheus Vinicius <matheus_viniciuss@outlook.com>
2022-12-23 16:32:15 -0300

    Validation

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -40,6 +40,13 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departmens = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departmens };
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -109,6 +116,13 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departmens = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departmens };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
